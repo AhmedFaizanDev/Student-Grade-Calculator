f72df8b48c2d910f9a3c6ea9e1d15d9da4ea86c8
AhmedFaizanDev <info2faizanahmed@gmail.com>
2024-07-16 22:15:49 +0530

    Added features to calculate median grade and sort grades

--- G-Calculator.cpp
+++ G-Calculator.cpp
@@ -5,11 +5,12 @@
 
 using namespace std;
 
+// Function to input grades
 void inputGrades(vector<int>& grades) {
     int grade;
 
     cout << "Enter student grades (enter -1 to finish):\n";
-
+    
     while (true) {
         if (!(cin >> grade)) {
             cout << "Invalid input. Please enter numeric grades only.\n";
@@ -20,10 +21,17 @@ void inputGrades(vector<int>& grades) {
 
         if (grade == -1)
             break;
+
+        if (grade < 0 || grade > 100) { // Example: valid grade range
+            cout << "Invalid grade entered. Grade must be between 0 and 100.\n";
+            continue;
+        }
+
         grades.push_back(grade);
     }
 }
 
+// Function to calculate statistics
 void calculateStatistics(const vector<int>& grades) {
     if (grades.empty()) {
         cout << "No grades entered. Exiting.\n";
@@ -44,11 +52,40 @@ void calculateStatistics(const vector<int>& grades) {
     cout << "Lowest grade: " << min_grade << "\n";
 }
 
+// Function to calculate median grade
+void calculateMedian(const vector<int>& grades) {
+    vector<int> sortedGrades = grades; // Copy grades to sort
+
+    sort(sortedGrades.begin(), sortedGrades.end());
+
+    double median;
+    if (sortedGrades.size() % 2 == 0) {
+        median = (sortedGrades[sortedGrades.size() / 2 - 1] + sortedGrades[sortedGrades.size() / 2]) / 2.0;
+    } else {
+        median = sortedGrades[sortedGrades.size() / 2];
+    }
+
+    cout << "Median grade: " << median << "\n";
+}
+
+// Function to sort grades
+void sortGrades(vector<int>& grades) {
+    sort(grades.begin(), grades.end());
+
+    cout << "Sorted grades:\n";
+    for (int grade : grades) {
+        cout << grade << " ";
+    }
+    cout << "\n";
+}
+
 int main() {
     vector<int> grades;
 
     inputGrades(grades);
     calculateStatistics(grades);
+    calculateMedian(grades);
+    sortGrades(grades);
 
     return 0;
 }
